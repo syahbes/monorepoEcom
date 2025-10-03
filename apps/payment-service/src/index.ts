131666eb65f47f0a25138bb328c741600f3cb1be
Shlomo Y <shlomo@techtio.io>
2025-10-03 08:04:13 +0300

    add hono

---
+++ apps/payment-service/src/index.ts
@@ -0,0 +1,27 @@
+import { serve } from '@hono/node-server';
+import { Hono } from 'hono';
+
+const app = new Hono();
+
+app.get('/', (c) => {
+  return c.text('Hello Hono!');
+});
+
+const start = async () => {
+  try {
+    serve(
+      {
+        fetch: app.fetch,
+        port: 8002,
+      },
+      (info) => {
+        console.log('Payment service is running on port 8002');
+      }
+    );
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+};
+
+start();
