b20052cf87ec78e5a2dbb4e73b85b1724c7dce07
Shlomo Y <shlomo@techtio.io>
2025-10-04 13:33:50 +0300

    auth with express

--- apps/client/src/app/layout.tsx
+++ apps/client/src/app/layout.tsx
@@ -1,23 +1,24 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
-import { ToastContainer } from "react-toastify";
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
+import { ToastContainer } from 'react-toastify';
+import { ClerkProvider } from '@clerk/nextjs';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-  title: "Trendlama - Best Clothes",
-  description: "Trendlama is the best place to find the best clothes",
+  title: 'Trendlama - Best Clothes',
+  description: 'Trendlama is the best place to find the best clothes',
 };
 
 export default function RootLayout({
@@ -26,17 +27,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <div className="mx-auto p-4 sm:px-0 sm:max-w-xl md:max-w-2xl lg:max-w-3xl xl:max-w-6xl">
-          <Navbar />
-          {children}
-          <Footer />
-        </div>
-        <ToastContainer position="bottom-right" />
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+          <div className="mx-auto p-4 sm:px-0 sm:max-w-xl md:max-w-2xl lg:max-w-3xl xl:max-w-6xl">
+            <Navbar />
+            {children}
+            <Footer />
+          </div>
+          <ToastContainer position="bottom-right" />
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
--- apps/product-service/src/index.ts
+++ apps/product-service/src/index.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
+import { clerkMiddleware, getAuth } from '@clerk/express';
 
 const app = express();
 app.use(
@@ -9,12 +10,27 @@ app.use(
   })
 );
 
-app.get("/health",(req: Request, res: Response) => {
+app.use(clerkMiddleware());
+
+app.get('/test/', (req: Request, res: Response) => {
+  const auth = getAuth(req);
+  const userId = auth.userId;
+
+  if (!userId) {
+    return res.status(401).json({
+      message: 'You are not logged in!',
+    });
+  }
+
+  return res.json({ message: 'Product service authenticated' });
+});
+
+app.get('/health', (req: Request, res: Response) => {
   return res.status(200).json({
     status: 'ok',
     uptime: process.uptime(),
     timstamp: Date.now(),
   });
-})
+});
 
 app.listen(8000, () => console.log('Product service is running on port 8000'));
