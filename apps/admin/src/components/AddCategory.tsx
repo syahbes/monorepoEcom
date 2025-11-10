8ae175cb4ad0190a3276c58e20c6a6e7a9bc55e0
Shlomo Y <shlomo@techtio.io>
2025-11-10 06:43:02 +0200

    add category react query

--- apps/admin/src/components/AddCategory.tsx
+++ apps/admin/src/components/AddCategory.tsx
@@ -1,41 +1,58 @@
-"use client";
+'use client';
 
-import {
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-} from "@/components/ui/sheet";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "./ui/form";
-import { Input } from "./ui/input";
-import { Button } from "./ui/button";
-
-const formSchema = z.object({
-  name: z.string().min(1, { message: "Name is Required!" }),
-});
+import { SheetContent, SheetDescription, SheetHeader, SheetTitle } from '@/components/ui/sheet';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from './ui/form';
+import { Input } from './ui/input';
+import { Button } from './ui/button';
+import { CategoryFormSchema } from '@repo/types';
+import { useMutation } from '@tanstack/react-query';
+import { useAuth } from '@clerk/nextjs';
+import { toast } from 'react-toastify';
 
 const AddCategory = () => {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const form = useForm<z.infer<typeof CategoryFormSchema>>({
+    resolver: zodResolver(CategoryFormSchema),
+    defaultValues: {
+      name: '',
+      slug: '',
+    },
   });
+
+  const { getToken } = useAuth();
+
+  const mutation = useMutation({
+    mutationFn: async (data: z.infer<typeof CategoryFormSchema>) => {
+      const token = await getToken();
+      const res = await fetch(`${process.env.NEXT_PUBLIC_PRODUCT_SERVICE_URL}/categories`, {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      if (!res.ok) {
+        throw new Error('Failed to create category!');
+      }
+    },
+    onSuccess: () => {
+      toast.success('Category created successfully');
+    },
+    onError: (error) => {
+      toast.error(error.message);
+    },
+  });
+
   return (
     <SheetContent>
       <SheetHeader>
         <SheetTitle className="mb-4">Add Category</SheetTitle>
         <SheetDescription asChild>
           <Form {...form}>
-            <form className="space-y-8">
+            <form className="space-y-8" onSubmit={form.handleSubmit((data) => mutation.mutate(data))}>
               <FormField
                 control={form.control}
                 name="name"
@@ -50,7 +67,23 @@ const AddCategory = () => {
                   </FormItem>
                 )}
               />
-              <Button type="submit">Submit</Button>
+              <FormField
+                control={form.control}
+                name="slug"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Slug</FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormDescription>Enter category slug.</FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit" disabled={mutation.isPending} className="disabled:opacity-50 disabled:cursor-not-allowed">
+                {mutation.isPending ? 'Submitting' : 'Submit'}
+              </Button>
             </form>
           </Form>
         </SheetDescription>
